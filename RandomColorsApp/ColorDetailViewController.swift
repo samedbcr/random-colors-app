bcd121a66716f7e5f3a0388a5c880e30581dc0d5
Samed Biçer <samedbcrr@gmail.com>
2021-05-10 17:35:43 +0300

    added convert uicolor to hexcode extension and added showing uicolor's hexcode into colordetailscreen

--- RandomColorsApp/ColorDetailViewController.swift
+++ RandomColorsApp/ColorDetailViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ColorDetailViewController: UIViewController {
 
+    @IBOutlet var colorHexCodeLabel: UILabel!
     var color: UIColor?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.navigationBar.tintColor = UIColor.white
-        view.backgroundColor = color ?? .systemOrange
+        guard let color = color else {
+            view.backgroundColor = .systemOrange
+            colorHexCodeLabel.text = "Invalid HEX Code"
+            return
+        }
+        navigationController?.navigationBar.tintColor = color.isDarkishColor ? .white : .black
+        view.backgroundColor = color
+        colorHexCodeLabel.text = color.toHexString()
+        colorHexCodeLabel.textColor = color.isDarkishColor ? .white : .black
     }
 }
--- RandomColorsApp/UIColor+Extensions.swift
+++ RandomColorsApp/UIColor+Extensions.swift
@@ -9,11 +9,38 @@ import UIKit
 
 extension UIColor {
 
+    /**
+     Generates a random color
+     */
     static func random() -> UIColor {
         let color = UIColor(red: CGFloat.random(in: 0...1),
                             green: CGFloat.random(in: 0...1),
                             blue: CGFloat.random(in: 0...1),
                             alpha: 1)
         return color
+    }
+    
+    /**
+     Converts UIColor to HEX String
+     */
+    func toHexString() -> String {
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb: Int = (Int)(r * 255) << 16 | (Int)(g * 255) << 8 | (Int)(b * 255) << 0
+        return String(format: "#%06x", rgb)
+    }
+    
+    /**
+     Determines if the UIColor darkish or not
+     */
+    var isDarkishColor: Bool {
+        var r, g, b, a: CGFloat
+        (r, g, b, a) = (0, 0, 0, 0)
+        self.getRed(&r, green: &g, blue: &b, alpha: &a)
+        let lum = 0.2126 * r + 0.7152 * g + 0.0722 * b
+        return  lum < 0.50
     }
 }
