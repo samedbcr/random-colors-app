ae7fe7b30753e9ae8b25bee2e62e3a9b7c6f49ed
samedbcr <bicerr252@gmai.com>
2021-05-10 22:36:14 +0300

    hexcode label changed to clickable uibutton, added copy to clipboard functionality and present alert after button click

--- RandomColorsApp/ColorDetailViewController.swift
+++ RandomColorsApp/ColorDetailViewController.swift
@@ -9,19 +9,39 @@ import UIKit
 
 class ColorDetailViewController: UIViewController {
 
-    @IBOutlet var colorHexCodeLabel: UILabel!
+    @IBOutlet var hexCodeButton: UIButton!
     var color: UIColor?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         guard let color = color else {
             view.backgroundColor = .systemOrange
-            colorHexCodeLabel.text = "Invalid HEX Code"
+            hexCodeButton.setTitle("Invalid HEX Code", for: .normal)
             return
         }
         navigationController?.navigationBar.tintColor = color.isDarkishColor ? .white : .black
         view.backgroundColor = color
-        colorHexCodeLabel.text = color.toHexString()
-        colorHexCodeLabel.textColor = color.isDarkishColor ? .white : .black
+        hexCodeButton.setTitle(color.toHexString(), for: .normal)
+        hexCodeButton.setTitleColor(color.isDarkishColor ? .white : .black, for: .normal)
+    }
+
+    @IBAction func hexCodeButtonClicked(_ sender: UIButton) {
+        if let color = color {
+            let pasteboard = UIPasteboard.general
+            pasteboard.string = color.toHexString()
+
+            presentAlert(as: "Copied!", with: "Color has copied to clipboard!")
+        }
+    }
+
+    func presentAlert(as title: String, with message: String) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        present(ac, animated: true)
+
+        // dismiss alert after 2 seconds
+        let when = DispatchTime.now() + 2
+        DispatchQueue.main.asyncAfter(deadline: when) {
+            ac.dismiss(animated: true, completion: nil)
+        }
     }
 }
